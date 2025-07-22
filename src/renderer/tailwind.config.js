c8022f17be293d626f09f61c4999582fc0b3d422
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2025-07-21 22:28:19 -0300

    chore: created electron-vite project with tailwind

---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/renderer/src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
